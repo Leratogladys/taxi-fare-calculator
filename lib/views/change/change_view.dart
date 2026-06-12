cff369d4750e3feebd04c7f98e809ce5d2155b62
Lerato Molefe <137295153+Leratogladys@users.noreply.github.com>
2026-06-12 17:18:39 +0200

    fix: add placeholder TrackingView and ChangeView to resolve undefined method errors

---
+++ lib/views/change/change_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChangeView extends StatelessWidget {
+  const ChangeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Change', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
---
+++ lib/views/tracking/tracking_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TrackingView extends StatelessWidget {
+  const TrackingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('History', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
